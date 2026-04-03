c66c48707b37fda032c9bf8d8d97cb3d8512de72
avlbrte <120362786+avlbrte@users.noreply.github.com>
2026-04-04 03:04:09 +0800

    Create switch.tsx

---
+++ components/ui/switch.tsx
@@ -0,0 +1,19 @@
+
+import * as React from "react"
+import * as SwitchPrimitives from "@radix-ui/react-switch"
+import { cn } from "@/lib/utils"
+
+export const Switch = React.forwardRef<
+  React.ElementRef<typeof SwitchPrimitives.Root>,
+  React.ComponentPropsWithoutRef<typeof SwitchPrimitives.Root>
+>(({ className, ...props }, ref) => (
+  <SwitchPrimitives.Root
+    className={cn(
+      "peer inline-flex h-6 w-11 shrink-0 cursor-pointer items-center rounded-full border transition-colors",
+      className
+    )}
+    {...props}
+    ref={ref}
+  />
+))
+Switch.displayName = SwitchPrimitives.Root.displayName
