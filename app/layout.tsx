b494833bb6de297789bfa17bcaf579f9cb85bb86
avlbrte <120362786+avlbrte@users.noreply.github.com>
2026-04-04 10:32:54 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,27 @@
 import "./globals.css";
+import type { Metadata } from "next";
 
-export const metadata = {
-  title: "Cafe POS",
-  description: "Simple cafe order app",
+export const metadata: Metadata = {
+  title: "SOKIE POS",
+  description: "Simple POS app for iPad/tablet",
 };
 
-<meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover">
-
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
+      <head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, viewport-fit=cover"
+        />
+      </head>
+
       <body>{children}</body>
     </html>
   );
 }
+
