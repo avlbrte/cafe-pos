bf977b60ced44043d329f5f733d64205da18bc8d
avlbrte <120362786+avlbrte@users.noreply.github.com>
2026-04-04 10:15:05 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,8 @@ export const metadata = {
   description: "Simple cafe order app",
 };
 
+<meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover">
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
